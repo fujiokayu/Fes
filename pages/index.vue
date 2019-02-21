2bab375f567d56cd294b192f3abc1cbb04df376d
fujiokayu <fujiokayu@gmail.com>
2019-02-21 21:59:59 +0900

    add file set function

--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,16 @@
       <h2 class="subtitle">
         a simple File Encription Service 
       </h2>
+      <!-- https://www.tohuandkonsome.site/entry/2018/01/22/223224#dropvue%E3%81%AB%E3%83%89%E3%83%A9%E3%83%83%E3%82%B0--%E3%83%89%E3%83%AD%E3%83%83%E3%83%97%E3%81%A7%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E9%81%B8%E6%8A%9E%E3%82%92%E8%BF%BD%E5%8A%A0%E3%81%99%E3%82%8B -->
+      <div class="drop" @dragleave.prevent @dragover.prevent @drop.prevent="onDrop">
+        <label> chose, or drag your file here: 
+          <input class="drop__input" type="file" multiple="multiple" @change="onDrop">
+        </label>
+      </div>
+
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://github.com/fujiokayu/Fes"
           target="_blank"
           class="button--grey">GitHub</a>
       </div>
@@ -26,6 +29,18 @@
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
+  methods:{
+  //input tag, or drag and drop will call; 
+    onDrop:function(event){
+      let fileList = event.target.files ? 
+                     event.target.files:
+                     event.dataTransfer.files;
+      let files = [];
+      for(let i = 0; i < fileList.length; i++){
+          files.push(fileList[i]);
+      }
+    }
+  },
   components: {
     AppLogo
   }
