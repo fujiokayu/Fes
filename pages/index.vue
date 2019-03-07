053292b6fac83974d5b1c0dc15d64f74efe4d385
fujiokayu <fujiokayu@gmail.com>
2019-03-07 20:44:25 +0900

    Rewrite README.md.
    and change meaning of F.

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         Fes
       </h1>
       <h2 class="subtitle">
-        simple File Encription Service 
+        Free Encryption Service 
       </h2>
       <!-- https://www.tohuandkonsome.site/entry/2018/01/22/223224#dropvue%E3%81%AB%E3%83%89%E3%83%A9%E3%83%83%E3%82%B0--%E3%83%89%E3%83%AD%E3%83%83%E3%83%97%E3%81%A7%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E9%81%B8%E6%8A%9E%E3%82%92%E8%BF%BD%E5%8A%A0%E3%81%99%E3%82%8B -->
       <div class="drop" @dragleave.prevent @dragover.prevent @drop.prevent="onDrop">
